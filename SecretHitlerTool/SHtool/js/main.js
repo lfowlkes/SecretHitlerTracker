0e6c809d248a52b191b4621845c9791266687583
Simon Katzer <skatzer@ucsc.edu>
2019-11-22 18:57:09 -0800

    Made it so the game works on Simon's computer!

--- SecretHitlerTool/SHtool/js/main.js
+++ SecretHitlerTool/SHtool/js/main.js
@@ -1,4 +1,14 @@
-var game = new Phaser.Game(1000,600, Phaser.AUTO, 'phaser');
+// Set up game
+var config = {
+    width: 1000,
+    height: 600,
+	parent: 'canvas-holder',
+    renderer: Phaser.AUTO,
+    antialias: true,
+    multiTexture: true,
+};
+
+var game = new Phaser.Game(config);
 
 var libCount = 6; //default count of liberal cards
 var facCount = 11; //default count of facist cards
@@ -14,8 +24,8 @@ preload: function() {
     game.load.image('lcard', 'assets/img/liberalcard.png');
     game.load.image('gamelogbg', 'assets/img/gamelogbg.png');
     game.load.image('arrow', 'assets/img/arrow.png');
-    
-    
+
+
 },
 
 create: function() {
@@ -23,12 +33,12 @@ create: function() {
     f1Active = true;
     f2Active = true;
     f3Active = true;
-    
+
     /*/ Adding game assests and text /*/
     game.stage.backgroundColor = '#eddca4';
     logbg = game.add.sprite(750, 0, 'gamelogbg')
     logbg.scale.setTo(5, 12);
-    
+
     /*/ Adding all card sprites /*/
     fcard1 = game.add.sprite(100, 200, 'fcard');
     fcard2 = game.add.sprite(300, 200, 'fcard');
@@ -36,7 +46,7 @@ create: function() {
     libcard1 = game.add.sprite(100, 200, 'lcard'); libcard1.alpha = 0; //liberal cards are originally hidden
     libcard2 = game.add.sprite(300, 200, 'lcard'); libcard2.alpha = 0;
     libcard3 = game.add.sprite(500, 200, 'lcard'); libcard3.alpha = 0;
-    
+
     /*/ Adding all arrow button sprites /*/
     arrow1Top = game.add.sprite(145, 165, 'arrow'); arrow1Top.inputEnabled = true;
     arrow2Top = game.add.sprite(345, 165, 'arrow'); arrow2Top.inputEnabled = true;
@@ -45,7 +55,7 @@ create: function() {
     arrow2Low = game.add.sprite(345, 485, 'arrow'); arrow2Low.inputEnabled = true;
     arrow3Low = game.add.sprite(545, 485, 'arrow'); arrow3Low.inputEnabled = true;
     arrow1Low.scale.setTo(1, -1); arrow2Low.scale.setTo(1, -1); arrow3Low.scale.setTo(1, -1);
-    
+
     /*/ Adding baseline game text /*/
     game.add.text(200, 25, 'Cards remaining:', { font: '40px Celtic Garamond the 2nd', fill: '#000'});
     game.add.text(120, 100, 'FACIST: ' + facCount, { font: '25px Arial', fill: '#000'});
@@ -54,7 +64,7 @@ create: function() {
 },
 
 update: function() {
-    
+
     /*/Event handlers for clicking on buttons/*/
     arrow1Top.events.onInputDown.add(this.card1Change, this);
     arrow1Low.events.onInputDown.add(this.card1Change,this);
@@ -63,7 +73,7 @@ update: function() {
     arrow3Top.events.onInputDown.add(this.card3Change, this);
     arrow3Low.events.onInputDown.add(this.card3Change, this);
 },
-    
+
     /*/ Event handler functions for changing the cards back and forth between liberal and facist as players click buttons /*/
 card1Change: function() {
     if(f1Active == true) //if the facist card is currently the one visible
@@ -72,7 +82,7 @@ card1Change: function() {
         fcard1.alpha = 0;
         libcard1.alpha = 1;
         f1Active = false;
-        
+
     }
     else //if the liberal card is currently the one visible
     {
@@ -82,7 +92,7 @@ card1Change: function() {
         f1Active = true;
     }
 },
-    
+
 card2Change: function() {
     if(f2Active == true) //if the facist card is currently the one visible
     {
@@ -99,7 +109,7 @@ card2Change: function() {
         f2Active = true;
     }
 },
-    
+
 card3Change: function() {
     if(f3Active == true) //if the facist card is currently the one visible
     {
@@ -123,17 +133,17 @@ card3Change: function() {
 
 var Conflict = function(game) {}; //game state to handle when the president and the chancellor claim to have given/received different cards and probabilities for both draws need to be logged and accounted for
 Conflict.prototype = {
-    
+
 preload: function() {
-    
+
 },
-    
+
 create: function() {
 
 },
-    
+
 update: function() {
-    
+
 }
 }
 
@@ -141,17 +151,17 @@ update: function() {
 
 var Random = function(game) {}; //Game state to log card play when government is thrown into choas and a random card is put on the board
 Random.prototype = {
-    
+
 preload: function() {
-    
+
 },
-    
+
 create: function() {
-    
+
 },
-    
+
 update: function() {
-    
+
 }
 }
 
